f1919720b48124fabab5ee1036df5559297ec72d
Himanshu Singh <87076645+HimanshuSingh-all@users.noreply.github.com>
2024-01-14 21:34:33 +0530

    Vey basics Barebones Window created with label. TODO: Createa A simple form using tk

---
+++ main.py
@@ -0,0 +1,16 @@
+import sympy as sp
+try:
+    import Tkinter as tk
+except: 
+    import tkinter as tk
+finally:
+    pass
+#from tkinter as tk
+
+root = tk.Tk()
+root.title("Differential Equation Solver")
+root.geometry('300x500')
+
+
+
+root.mainloop()
